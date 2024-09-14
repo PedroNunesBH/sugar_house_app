fb927b3b55eb92d0cbf8bd53f4465546fd2df720
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-09-14 17:51:38 -0300

    feat: add /sobre-nos/ page

--- confectionery/urls.py
+++ confectionery/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from confectionery.views import HomeView
+from confectionery.views import HomeView, AboutUsView
 
 urlpatterns = [
-    path("", HomeView.as_view(), name="home")
+    path("", HomeView.as_view(), name="home"),
+    path("sobre-nos/", AboutUsView.as_view(), name="sobre_nos")
 ]
--- confectionery/views.py
+++ confectionery/views.py
@@ -3,4 +3,8 @@ from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
-    template_name = "index.html"
+    template_name = "base.html"
+
+
+class AboutUsView(TemplateView):
+    template_name = "about_us.html"
