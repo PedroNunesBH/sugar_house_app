fc4dc8ff4e221a88ead6e17ad067b85f3668ee08
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-09-15 16:00:46 -0300

    fix: correct message field in contact form

--- confectionery/forms.py
+++ confectionery/forms.py
@@ -4,4 +4,5 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=200)
     email = forms.EmailField()
-    message = forms.Textarea()
+    message = forms.CharField(widget=forms.Textarea)
+    
