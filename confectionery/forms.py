f14aa5b31e40e591c5fd8ffcafad3f9f1ccf9120
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-09-15 16:25:03 -0300

    fix: add labels to contact form fields

--- confectionery/forms.py
+++ confectionery/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=200)
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)
+    name = forms.CharField(max_length=200, label="Nome")
+    email = forms.EmailField(label="Email")
+    message = forms.CharField(widget=forms.Textarea, label="Mensagem")
     
